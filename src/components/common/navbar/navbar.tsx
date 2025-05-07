c444e638cf1eed2fb698752c52fd21bf38a01d6d
Lukas <lukasvdgaag@gmail.com>
2025-05-07 14:15:01 +0200

    fix: Navbar on plugin list page

--- src/components/common/navbar/navbar.tsx
+++ src/components/common/navbar/navbar.tsx
@@ -2,8 +2,6 @@ import React, {FC} from 'react';
 import classNames from "classnames";
 import Link from "next/link";
 import {NavbarItem} from "@/components/common/navbar/features/navbar-item";
-import {LogoIcon} from "@/components/common/icon/logo-icon";
-import css from "@/components/common/navbar/navbar.module.scss";
 import {NavbarActionButton} from "@/components/common/navbar/features/navbar-action-button";
 import {useSessionHook} from "@/hooks/use-session-hook";
 
@@ -14,17 +12,15 @@ interface NavbarProps {
 export const Navbar: FC<NavbarProps> = ({background = false}) => {
     const {session} = useSessionHook();
 
-    console.log(session)
-
     return (
         <header className={classNames({'header-filled': background}, 'w-full z-10 justify-center max-w-screen-xl px-3 md:px-6 lg:px-10')}>
-            <nav className="h-20 flex flex-row justify-between items-center">
+            <nav className="flex flex-row justify-between items-center">
                 <Link href="/" className="w-56 plain text-white text-2xl font-bold">
                     {/*<LogoIcon className={classNames(css.headerLogo, background && css.headerLogofillBlack)}/>*/}
                     Devmart
                 </Link>
 
-                <div>
+                <div className="flex">
                     <NavbarItem label="Home" href="/" background={background}/>
                     <NavbarItem label="Paste" href="/paste" background={background}/>
                     <NavbarItem label="Plugins" href="/plugins" background={background}/>
